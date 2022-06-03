dd8f630f931191383938e9897cb33cf5b526f8f7
Xenometon <106739081+Xenometon@users.noreply.github.com>
2022-06-03 18:15:54 +0530

    Add files via upload

---
+++ SDG.py
@@ -0,0 +1,194 @@
+#This is Python program to display all the Sustainable Development Goals (SDG's)
+#And the role of Artificial Intelligence (AI) in acheiving that goal
+
+print("This program displays all the SDG's and the role of Artificial Intelligence in acheiving them.")
+print("Following are the various Sustainable Development goals (SDG's)-")
+print("Choose one from the following in which you want see the key role of AI helping to achieve that-")
+print("Enter the alphabet alongside the SDG's to select them as options.")
+print("Following are the 17 Sustainable Developmental Goals in the world-")
+SDG1="1. No poverty"
+SDG2="2. Zero Hunger"
+SDG3="3. Good Health & Well - Being"
+SDG4="4. Quality Education"
+SDG5="5. Gender Equality"
+SDG6="6. Clean Water & Sanitation"
+SDG7="7. Affordable and clean energy"
+SDG8="8. Decent work and economic growth"
+SDG9="9. Industry Innovation & infrastructure"
+SDG10="10. Reduced Inequalities"
+SDG11="11. Sustainable cities and communities"
+SDG12="12. Responsible consumption and production"
+SDG13="13. Climate change"
+SDG14="14. Life below water"
+SDG15="15. Life on land"
+SDG16="16. Peace, Justice & strong Institution"
+SDG17="17. Partnership for the Goals"
+print(SDG1)
+print(SDG2)
+print(SDG3)
+print(SDG4)
+print(SDG5)
+print(SDG6)
+print(SDG7)
+print(SDG8)
+print(SDG9)
+print(SDG10)
+print(SDG11)
+print(SDG12)
+print(SDG13)
+print(SDG14)
+print(SDG15)
+print(SDG16)
+print(SDG17)
+x=int(input("Choose your number from 1-17 to display the respective information:"))
+print("The number you chose:",x)
+if x==1:
+    print("Sustainable Developmental goal (SDG)- 1")
+    print("-:No poverty:-")
+    print("Role of AI-")
+    print("• AI can identify the most needy regions.")
+    print("• Ways to increase new skill suitable to the surroundings")
+    print("• Improving Agriculture")
+    print("• Improving Education Levels")
+elif x==2:
+    print("Sustainable Developmental goal (SDG)-2")
+    print("-:Zero Hunger:-")
+    print("Role of AI-")
+    print("• Efficient food production- soil identification")
+    print("• Predictions for future fragile ecosystem")
+    print("• Impact of global warming")
+    print("• Growing perfect Crop")
+    print("• Maximizing current output-usage of good and bad crop product")
+    print("• Precise application of Herbicides")
+    print("• Early warning of a crisis")
+elif x==3:
+    print("Sustainable Developmental goal (SDG)-3")
+    print("-:Good Health & Well-Being:-")
+    print("Role of AI-")
+    print("• Health analysis and treatment")
+    print("• Research and Training")
+    print("• Early detection and diagnosis")
+elif x==4:
+    print("Sustainable Developmental goal (SDG)-4")
+    print("-:Quality Education:-")
+    print("Role of AI-")
+    print("• Access to the internet")
+    print("• Personalized learning -text/audio/video/action oriented")
+    print("• Customizing assignment and assessments as well")
+    print("• Intelligent supportive learning environments")
+    print("• Simplifying Administrative tasks")
+    print("• Smart content- 3D visualization/customize books")
+    print("• Personalized learning, personalized tutor, NLP processing and progress analysis.")
+elif x==5:
+    print("Sustainable Developmental goal (SDG)-5")
+    print("-:Gender Equality:-")
+    print("Role of AI-")
+    print("• Addressing wage disparity")
+    print("• Remove pay disparity")
+    print("• Insights the causes behind these gaps")
+    print("• Data driven decision on opportunities")
+    print("• Detecting online sexual harassment")
+elif x==6:
+    print("Sustainable Developmental goal (SDG)-6")
+    print("-:Clean Water & Sanitation:-")
+    print("Role of AI-")
+    print("• Detect dangerous bacteria and harmful particles/pathogens in water")
+    print("• Inspecting the water quality( uses pattern recognition and ML)")
+    print("• Marks contaminated sites")
+    print("• Water treatment plant powered by AI")
+    print("• Accurate and Timely information about the pollutants and recommendations for treatment")
+elif x==7:
+    print("Sustainable Developmental goal (SDG)-7")
+    print("-:Affordable and clean energy:-")
+    print("Role of AI-")
+    print("• Smart centralized control centres")
+    print("• Improved integration of microgrids")
+    print("• Improved safety and reliability")
+    print("• Expanding the market")
+    print("• Smart Grid and Intelligent storage")
+elif x==8:
+    print("Sustainable Developmental goal (SDG)-8")
+    print("-:Decent work and economic growth:-")
+    print("Role of AI-")
+    print("• Skills for jobs of the future")
+    print("• Financial Access- usage of money in proper way")
+    print("• Insufficient labour demand")
+elif x==9:
+    print("Sustainable Developmental goal (SDG)-9")
+    print("-:Industry Innovation & infrastructure:-")
+    print("Role of AI-")
+    print("• Analysing Structural defects")
+    print("• Landscape designing")
+    print("• Waste management")
+    print("• Smart recycling")
+elif x==10:
+    print("Sustainable Developmental goal (SDG)-10")
+    print("-:Reduced Inequalities:-")
+    print("Role of AI-")
+    print("• Tailored Technology- affordable facility")
+    print("• Social Media and crowdsourcing platforms")
+    print("• Helping refugees- chatbots")
+    print("• Automating justice and equal employment- uses machine learning for predicting the good candidates")
+    print("• Creating education accessibility- low cost")
+    print("• Helping visually Impaired to navigate to their environment")
+elif x==11:
+    print("Sustainable Developmental goal (SDG)-11")
+    print("-:Sustainable cities and communities:-")
+    print("Role of AI-")
+    print("• Max efficient usage of existing resources")
+    print("• Planning and zoning")
+    print("• Planning for scalability & sustainability (feasibility and upscaling)")
+elif x==12:
+    print("Sustainable Developmental goal (SDG)-12")
+    print("-:Responsible consumption and production:-")
+    print("Role of AI-")
+    print("• Business model and production innovation")
+    print("• Precision Recycling")
+    print("• Material tracking system (RFID/IoT sensors max usage)")
+    print("• 3D printing")
+elif x==13:
+    print("Sustainable Developmental goal (SDG)-13")
+    print("-:Climate change:-")
+    print("Role of AI-")
+    print("• Climate study and Analysis")
+    print("• Development of better solution")
+    print("• Green initiatives")
+    print("• Better weather predictions and forecast")
+elif x==14:
+    print("Sustainable Developmental goal (SDG)-14")
+    print("-:Life below water:-")
+    print("Role of AI-")
+    print("• Monitoring and regulating of illegal fishing")
+    print("• Traceability and identification of species")
+    print("• Smart aquaculture")
+    print("• Environmental monitoring and Smart Conservation")
+    print("• Tracking Fishing vessels")
+    print("• Ocean cleaning")
+elif x==15:
+    print("Sustainable Developmental goal (SDG)-15")
+    print("-:Life on land:-")
+    print("Role of AI-")
+    print("• Preventing Wildfires")
+    print("• Checking Deforestation")
+    print("• Spotting & Stopping Poacher")
+elif x==16:
+    print("Sustainable Developmental goal (SDG)-16")
+    print("-:Peace, Justice & Strong Institution:-")
+    print("Role of AI-")
+    print("• Blockchain (detect the origin of product eg coffee beans)")
+    print("• Intelligent Cybersecurity")
+    print("• Automating Processes- online tax collection, land rights")
+    print("• E- Governance")
+    print("Detecting suspicious behaviours")
+elif x==17:
+    print("Sustainable Developmental goal (SDG)-17")
+    print("-:Partnership for the Goals:-")
+    print("Role of AI-")
+    print("• Digital collaborations")
+    print("• Measurement and tracking")
+    print("• Applications of Artificial Intelligence")
+else:
+    print("Invalid input!")
+print("----------------")
+
+#End of the program#
